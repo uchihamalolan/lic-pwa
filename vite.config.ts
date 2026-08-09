d2b852110915018ccf1b4cf5d95e4549954994fa
Malolan B <ma101an@icloud.com>
2026-08-09 23:16:43 +0530

    Step 2: Add StyleX

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import stylex from "@stylexjs/unplugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [stylex.unplugin.vite(), react()],
 });
